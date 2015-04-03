882af4cece4a006fdbae4a9eeb9a0e3ed1dc8fde
Andrey Starkov <im@andreystarkov.ru>
2015-04-03 17:33:18 +0500

    cleaner

--- src/js/init.js
+++ src/js/init.js
@@ -67,11 +67,20 @@
 		}
 	});
 
+    function createSliderNavigation(obj){
+        var box = $(obj);
+        var count = $('.item', obj).length;
+        for(var i = 0; i < count; i++){
+            $('.carousel-indicators', box).append('<li data-target="'+obj+'" data-slide-to="'+i+'"></li>');
+        }
+        $(box).append('<div class="left carousel-control carousel-left" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Previous</span></div><div class="right carousel-control carousel-right" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><span class="sr-only">Next</span></div>');
+    }
+
     function animate(element_ID, animation) {
 
         $(element_ID).css({'display': 'block'}).addClass(animation);
         var wait = window.setTimeout( function(){
-            $(element_ID).removeClass(animation)}, 1300
+            $(element_ID).removeClass(animation)}, 1000
         );
     }
 
@@ -85,9 +94,14 @@
 
 	$(function() {
 
-        new WOW().init();
+		addAnimation('.box-features section', 'fadeInUp');
+	//	addAnimation('.news-item', 'fadeInLeft');
 
-		addAnimation('.box-features section', 'bounceInUp');
+		wow = new WOW({
+		  boxClass:'wow', animateClass: 'animated', offset: 0, mobile: true, live: true
+		});
+
+		wow.init();
 
 		$('body').flowtype({minFont: 12, maxFont: 16, maximum: 1200});
 
@@ -106,6 +120,29 @@
 			$(this).addClass('animated');
 		});
 
+		$('.carousel').carousel({ interval: 15000, pause: 'hover' });
+
+		createSliderNavigation('#slider-header');
+
+	    $('.carousel-left').click(function(){
+	        $(this).parent().carousel('prev');
+	    });
+	    $('.carousel-right').click(function(){
+	        $(this).parent().carousel('next');
+	    });
+
+        window.setTimeout( function(){
+          //  $(".carousel-caption").removeClass('fadeInUp').attr({"data-wow-duration": "0.9s", "data-wow-delay": "0.2s"});
+        }, 2000);
+
+		$('#slider-header').on('slide.bs.carousel', function () {
+		  	$('.carousel-caption').fadeOut();
+		});
+
+		$('#slider-header').on('slid.bs.carousel', function () {
+			 animate(".carousel-caption", 'rotateInUpLeft');
+		});
+
 		var	$window = $(window),
 			$body = $('body');
 
@@ -114,16 +151,6 @@
 			$window.on('load', function() {
 				$body.removeClass('is-loading');
 			});
-
-			$('.carousel').carousel({ interval: 15000, pause: 'hover' });
-
-			$('#slider-header').on('slide.bs.carousel', function () {
-			  	$('.carousel-caption').fadeOut();
-			})
-
-			$('#slider-header').on('slid.bs.carousel', function () {
-				 animate(".carousel-caption", 'bounceInUp');
-			})
 
 	});
 
